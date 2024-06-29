b3c397b4707bbc0cfee79b6ab1e0356437fced3f
chenqian <qianpou@163.com>
2024-06-29 17:21:42 +0800

    CSS代码提示补充

--- bundles/com.tulin.v8.webtools.ide/src/com/tulin/v8/webtools/ide/css/CSSAssistProcessor.java
+++ bundles/com.tulin.v8.webtools.ide/src/com/tulin/v8/webtools/ide/css/CSSAssistProcessor.java
@@ -25,35 +25,65 @@ public class CSSAssistProcessor implements IContentAssistProcessor {
 		String word = getLastWord(text);
 		List<ICompletionProposal> list = new ArrayList<ICompletionProposal>();
 		if (word != null) {
-			if (word.indexOf(":") > 0) {
-				String prop = word.substring(0, word.indexOf(":"));
-				String vals = word.substring(word.indexOf(":") + 1).trim();
-				for (int i = 0; i < CSSDefinition.CSS_VALUES.length; i++) {
-					if (CSSDefinition.CSS_VALUES[i].getName().startsWith(prop)) {
-						List<CSSInfo> values = CSSDefinition.CSS_VALUES[i].getValues();
-						for (CSSInfo value : values) {
-							if (value.getReplaceString().startsWith(vals)) {
-								list.add(new CompletionProposal(value.getReplaceString() + ";", offset - vals.length(),
-										vals.length(), value.getReplaceString().length() + 1,
-										WebToolsPlugin.getDefault().getImageRegistry()
-												.get(WebToolsPlugin.ICON_CSS_PROP),
-										value.getDisplayString(), null, value.getDescription()));
-							}
+			if (word.endsWith("{")) {
+				word = word.substring(0, word.length() - 1);
+				if (word.indexOf(":") > 0) {
+					String vals = word.substring(word.indexOf(":") + 1).trim();
+					for (CSSInfo value : CSSDefinition.Pseudo_Element) {
+						if (value.getReplaceString().startsWith(vals)) {
+							String replaceString = value.getReplaceString();
+							String description = value.getDescription();
+							list.add(new CompletionProposal(replaceString, offset - vals.length(), vals.length(),
+									replaceString.length(),
+									WebToolsPlugin.getDefault().getImageRegistry().get(WebToolsPlugin.ICON_CSS_PROP),
+									replaceString, null, description));
+						}
+					}
+				} else if (word.indexOf("@") > -1) {
+					String vals = word.substring(word.indexOf("@")).trim();
+					for (CSSInfo value : CSSDefinition.Media_Type) {
+						if (value.getReplaceString().startsWith(vals)) {
+							String replaceString = value.getReplaceString();
+							String description = value.getDescription();
+							list.add(new CompletionProposal(replaceString, offset - vals.length(), vals.length(),
+									replaceString.length(),
+									WebToolsPlugin.getDefault().getImageRegistry().get(WebToolsPlugin.ICON_CSS_PROP),
+									replaceString, null, description));
 						}
 					}
 				}
 			} else {
-				for (int i = 0; i < CSSDefinition.CSS_KEYWORDS.length; i++) {
-					if (CSSDefinition.CSS_KEYWORDS[i].getReplaceString().startsWith(word)) {
-						String replaceString = CSSDefinition.CSS_KEYWORDS[i].getReplaceString();
-						if (replaceString.indexOf(":") < 0) {
-							replaceString += ": ";
+				if (word.indexOf(":") > 0) {
+					String prop = word.substring(0, word.indexOf(":"));
+					String vals = word.substring(word.indexOf(":") + 1).trim();
+					for (int i = 0; i < CSSDefinition.CSS_VALUES.length; i++) {
+						if (CSSDefinition.CSS_VALUES[i].getName().startsWith(prop)) {
+							List<CSSInfo> values = CSSDefinition.CSS_VALUES[i].getValues();
+							for (CSSInfo value : values) {
+								if (value.getReplaceString().startsWith(vals)) {
+									list.add(new CompletionProposal(value.getReplaceString() + ";",
+											offset - vals.length(), vals.length(),
+											value.getReplaceString().length() + 1,
+											WebToolsPlugin.getDefault().getImageRegistry()
+													.get(WebToolsPlugin.ICON_CSS_PROP),
+											value.getDisplayString(), null, value.getDescription()));
+								}
+							}
 						}
-						list.add(new CompletionProposal(replaceString, offset - word.length(), word.length(),
-								replaceString.length(),
-								WebToolsPlugin.getDefault().getImageRegistry().get(WebToolsPlugin.ICON_CSS_PROP),
-								CSSDefinition.CSS_KEYWORDS[i].getDisplayString(), null,
-								CSSDefinition.CSS_KEYWORDS[i].getDescription()));
+					}
+				} else {
+					for (int i = 0; i < CSSDefinition.CSS_KEYWORDS.length; i++) {
+						if (CSSDefinition.CSS_KEYWORDS[i].getReplaceString().startsWith(word)) {
+							String replaceString = CSSDefinition.CSS_KEYWORDS[i].getReplaceString();
+							if (replaceString.indexOf(":") < 0) {
+								replaceString += ": ";
+							}
+							list.add(new CompletionProposal(replaceString, offset - word.length(), word.length(),
+									replaceString.length(),
+									WebToolsPlugin.getDefault().getImageRegistry().get(WebToolsPlugin.ICON_CSS_PROP),
+									CSSDefinition.CSS_KEYWORDS[i].getDisplayString(), null,
+									CSSDefinition.CSS_KEYWORDS[i].getDescription()));
+						}
 					}
 				}
 			}
@@ -70,8 +100,12 @@ public class CSSAssistProcessor implements IContentAssistProcessor {
 	}
 
 	private String getLastWord(String text) {
-
 		text = HTMLUtil.cssComment2space(text);
+
+		int index0 = text.lastIndexOf('}');
+		if (index0 < text.length() - 1) {
+			text = text.substring(index0 + 1);
+		}
 
 		int index1 = text.lastIndexOf(';');
 		int index2 = text.lastIndexOf('{');
@@ -80,9 +114,9 @@ public class CSSAssistProcessor implements IContentAssistProcessor {
 			return text.substring(index1 + 1).trim();
 		} else if (index2 >= 0) {
 			return text.substring(index2 + 1).trim();
+		} else {
+			return text.trim() + "{";
 		}
-
-		return null;
 	}
 
 	public IContextInformation[] computeContextInformation(ITextViewer viewer, int offset) {
